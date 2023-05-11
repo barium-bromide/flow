15afa3e9bf15e0b873aa210d010365e118fb492e
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2023-05-11 17:35:20 +0800

    output and defined a variable

--- main.py
+++ main.py
@@ -17,13 +17,22 @@ with open(args.input_file, 'r') as f:
 
 for line in lines:
 	# TODO: do something with lines
-	line = "".join(line.splitlines())
-	if line[0:4] == "say,": 
-		compiled_code += f"console.log(\"{line[4:]}\");\n"
-	elif line[0:5] == "hear,":
-		compiled_code += f"prompt(\"{line[5:]}\")"
-	else:
-		compiled_code += "console.log(\"a\")"
+	line = line.strip()
+	token = line.split(" ")
+	if token[0] == "say" and token[-1][-1] == ";":
+		token[-1] = token[-1][:-1] + ""
+		compiled_code += f'console.log(\"{" ".join(token[1:])}\");\n'
+	elif token[0] in ("bool", "str", "num") and token[-1][-1] == ";":
+			token[-1] = token[-1][:-1] + ""
+			var_token = "".join(token[1:]).split("=")
+			if token[0] == "str":
+				compiled_code += f'let {var_token[0]} = \"{"=".join(var_token[1:])}\";\n'
+			elif token[0] == "num":
+				if "=".join(var_token[1:]).isnumeric():
+					compiled_code += f'let {var_token[0]} = {"=".join(var_token[1:])};\n'
+			elif token[0] == "bool":
+				if "=".join(var_token[1:]) in ("true", "false"):
+					compiled_code += f'let {var_token[0]} = {"=".join(var_token[1:])};\n'
 
 with open(args.output_file, 'w') as f:
 	f.write(compiled_code)
